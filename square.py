7f2316ff3977de10c52b4372a27d2aae343da669
none <none>
2017-01-10 19:02:18 +0200

    stopping here

---
+++ square.py
@@ -0,0 +1,13 @@
+from rectangle import Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, length):
+        super(Square, self).__init__(length,length)
+
+    def set_length(self,new_length):
+        if new_length>=0 :
+            self.length=new_length
+            if self.has_been_drawn :
+                self.draw_shape()
+                
