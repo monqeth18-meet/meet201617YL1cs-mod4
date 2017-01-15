8227673d9a554ef963b65fa1f0e761aa480836f9
monqeth18-meet <none>
2017-01-15 20:06:53 +0200

    stopping here

--- square.py
+++ square.py
@@ -10,6 +10,25 @@ class Square(Rectangle):
             self.length=new_length
             if self.has_been_drawn :
                 self.draw_shape()
-                
 
     
+    def draw_shape(self,x,y):
+        """
+        Draw the shape, starting at 0,0.
+
+        If any old drawings exist, remove them.
+        """
+        x = 
+        
+        self.turtle.clear() #Remove old drawings (if they exist)
+        self.turtle.penup()
+        self.turtle.goto(x,y)
+        self.turtle.pendown()
+        self.turtle.goto(self.length,y)
+        self.turtle.goto(self.length,self.length)
+        self.turtle.goto(x,self.length)
+        self.turtle.goto(x,y)
+        self.turtle.penup()
+        self.has_been_drawn=True
+
+
